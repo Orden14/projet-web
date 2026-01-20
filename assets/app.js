b2bae502b9a6081cb4394b2586e8c7036f557be3
Orden14 <orden0614@gmail.com>
2026-01-20 14:05:44 +0100

    remove datatables

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,5 @@ import "./bootstrap.js";
 import "./styles/global.scss";
 
 import "bootstrap-select/dist/js/bootstrap-select.js";
-import "jquery-confirm/js/jquery-confirm.js";
 
-import "datatables.net-bs5";
-import "datatables.net-select-bs5";
+import "jquery-confirm/js/jquery-confirm.js";
