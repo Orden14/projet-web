6617842af34cfa6533b874dcc3b65ba4a689a59f
Orden14 <orden0614@gmail.com>
2026-01-20 09:24:51 +0100

    add relational entities

---
+++ src/Entity/Tag.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TagRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TagRepository::class)]
+class Tag
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private string $title;
+
+    #[ORM\Column(length: 255)]
+    private string $color;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private User $owner;
+
+    final public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    final public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    final public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    final public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    final public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    final public function getOwner(): User
+    {
+        return $this->owner;
+    }
+
+    final public function setOwner(User $owner): self
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+}
